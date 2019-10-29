3f11d8ecb5c9170ece39bbf71925b506b36116d5
loferris <lo.k.ferris@gmail.com>
2019-10-28 18:59:52 -0700

    refactoring for buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "../components/layout";
 //import Canvas from "../components/canvas";
 //import CustomImageEditor from "../components/CustomImageEditor";
 //import Editor from '../components/editor';
 import Imgix from "react-imgix";
 
-export default () => (
+export default class App extends Component { 
+  state = {
+    src: 'https://grimoire.imgix.net/lupines-jenner.jpeg?txt=the%20coast&txt-color=white&txt-size=300&txt-align=bottom%2Ccenter&w=600&txt-font=monospace',
+    vibrant: { auto: 'enhance', sat: 50, con: 25, fit: 'crop' }, 
+    classic: { auto: 'enhance', fit: 'crop', sat: 50, con: 25, monochrome: 484646 },
+    vintage: { auto: 'enhance', fit: 'crop', sat: 50, con: 25, sepia: 70 },
+    original: { auto: 'enhance', fit: 'crop' },
+    imgixParams: { auto: 'enhance', fit: 'crop' }
+  }
+
+  handleClickVibrant = e => {
+    this.setState({ imgixParams: this.state.vibrant })
+  }
+  render() {
+  return (
   <Layout>
     <div>
     <Imgix
-      src='https://grimoire.imgix.net/lupines-jenner.jpeg?txt=the%20coast&txt-color=white&txt-size=300&txt-align=bottom%2Ccenter&w=600&txt-font=monospace'
-      imgixParams={{ auto: 'enhance', sat: 50, con: 25, fit: 'crop' }}
+      src={this.state.src}
+      imgixParams={this.state.vibrant}
     />
     <Imgix
-      src='https://grimoire.imgix.net/lupines-jenner.jpeg?txt=the%20coast&txt-color=white&txt-size=300&txt-align=bottom%2Ccenter&w=600&txt-font=monospace'
-      imgixParams={{ auto: 'enhance', fit: 'crop', sat: 50, con: 25, monochrome: 484646 }}
+      src={this.state.src}
+      imgixParams={this.state.classic}
     />
     <Imgix
-          src='https://grimoire.imgix.net/lupines-jenner.jpeg?txt=the%20coast&txt-color=white&txt-size=300&txt-align=bottom%2Ccenter&w=600&txt-font=monospace'
-          imgixParams={{ auto: 'enhance', fit: 'crop', sat: 50, con: 25, sepia: 70 }}
+          src={this.state.src}
+          imgixParams={this.state.vintage}
         />
     <Imgix
-        src='https://grimoire.imgix.net/lupines-jenner.jpeg?txt=the%20coast&txt-color=white&txt-size=300&txt-align=bottom%2Ccenter&w=600&txt-font=monospace'
-        imgixParams={{ auto: 'enhance', fit: 'crop' }}
+        src={this.state.src}
+        imgixParams={this.state.original}
       />
     <button>original</button>
     <button>vibrant</button>
@@ -30,4 +44,5 @@ export default () => (
     <button>vintage</button>
     </div>
   </Layout>
-);
+  )}
+};
