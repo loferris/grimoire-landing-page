1cfdde26a63c615340446a19d19edecd1875ca95
loferris <lo.k.ferris@gmail.com>
2019-10-28 19:11:47 -0700

    buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,40 +8,37 @@ import Imgix from "react-imgix";
 export default class App extends Component { 
   state = {
     src: 'https://grimoire.imgix.net/lupines-jenner.jpeg?txt=the%20coast&txt-color=white&txt-size=300&txt-align=bottom%2Ccenter&w=600&txt-font=monospace',
-    vibrant: { auto: 'enhance', sat: 50, con: 25, fit: 'crop' }, 
-    classic: { auto: 'enhance', fit: 'crop', sat: 50, con: 25, monochrome: 484646 },
-    vintage: { auto: 'enhance', fit: 'crop', sat: 50, con: 25, sepia: 70 },
-    original: { auto: 'enhance', fit: 'crop' },
     imgixParams: { auto: 'enhance', fit: 'crop' }
   }
 
   handleClickVibrant = e => {
-    this.setState({ imgixParams: this.state.vibrant })
+    this.setState({ imgixParams: { auto: 'enhance', sat: 50, con: 25, fit: 'crop' } })
   }
+
+  handleClickClassic = e => {
+      this.setState({ imgixParams: { auto: 'enhance', fit: 'crop', sat: 50, con: 25, monochrome: 484646 } })
+    }
+
+  handleClickVintage = e => {
+    this.setState({ imgixParams: { auto: 'enhance', fit: 'crop', sat: 50, con: 25, sepia: 70 } })
+  }
+
+  handleClickOriginal = e => {
+    this.setState({ imgixParams: { auto: 'enhance', fit: 'crop' } })
+  }
+
   render() {
   return (
   <Layout>
     <div>
     <Imgix
       src={this.state.src}
-      imgixParams={this.state.vibrant}
+      imgixParams={this.state.imgixParams}
     />
-    <Imgix
-      src={this.state.src}
-      imgixParams={this.state.classic}
-    />
-    <Imgix
-          src={this.state.src}
-          imgixParams={this.state.vintage}
-        />
-    <Imgix
-        src={this.state.src}
-        imgixParams={this.state.original}
-      />
-    <button>original</button>
-    <button>vibrant</button>
-    <button>classic</button>
-    <button>vintage</button>
+    <button onClick={this.handleClickOriginal}>original</button>
+    <button onClick={this.handleClickVibrant}>vibrant</button>
+    <button onClick={this.handleClickClassic}>classic</button>
+    <button onClick={this.handleClickVintage}>vintage</button>
     </div>
   </Layout>
   )}
